0835489fbee1d2e2e1f4953523109856f576718f
Osondu Dominion <domikdegreat@gmail.com>
2022-05-17 14:30:19 +0100

    for all

---
+++ Capstone_Project_group_156_B2/sign_up_form/form-script.js
@@ -0,0 +1,99 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+    
+    
+        let counter = document.querySelector('button');
+    let count = 1;
+    
+    
+    setInterval(()=>{
+        counter.innerText = count;
+        count++
+
+        if (count > 5) location.replace('./sample.html');
+    },1000)
+
+    
+    
+    
+   
+    
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function passwordBtn() {
+    var x = document.getElementById("password")
+    var passItem1 = document.getElementById("check")
+    var passItem2 = document.getElementById("password")
+
+    if (x.type === 'password'){
+        passItem1.addEventListener("click", () => {
+        passItem.Type === "text"
+    })
+    }
+
+    else{
+        passItem1.addEventListener("click", () => {
+            passItem.Type === "password"
+        })
+    }
+}
